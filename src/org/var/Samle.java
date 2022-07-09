5825948497feddd6253cf8b6a976eb46eff1e685
Karthi1396 <ppandikarthi3@gmail.com>
2022-07-08 23:50:02 -0700

    hello

---
+++ src/org/var/Samle.java
@@ -0,0 +1,23 @@
+package org.var;
+
+public class Samle {
+	 int id=10;
+	private void k() {
+		int id=100;
+		System.out.println(id);
+	}
+	private void v() {
+		System.out.println(id);
+	}
+	public static void main(String[] args) {
+		Samle a=new Samle();
+		a.k();
+		a.v();
+		Samle b=new Samle();
+		b.v();
+		b.k();
+				
+
+	}
+	
+}
